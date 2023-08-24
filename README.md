9647c75de86773248c9f8dee62b436ef0022d36e
vini051 <60819460+vini051@users.noreply.github.com>
2023-08-24 02:52:33 +0000

    added `lint:check` & `lint:fix` scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendendo a implementar um projeto, do zero, de fora a fora com o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
-export default function Home(){
-    return <h1>Ela sabe</h1>
+export default function Home() {
+  return <h1>Ela sabe</h1>;
+}
+
+function teste() {
+  console.log("prettier esta funcionando!");
 }
