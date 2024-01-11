1d575af82f8a9498c759c5905f354d67f21e5598
vini051 <60819460+vini051@users.noreply.github.com>
2024-01-11 21:54:23 +0000

    add server version, max connections and current connections in status return of API

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,21 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 2 as sum;");
-  console.log(result.rows);
-  response.status(200).send({ chave: "queijo foudasi" });
+  const updatedAt = new Date().toISOString();
+  const getVersion = await database.query(
+    "SELECT current_setting('server_version') as server_version;",
+  );
+  const maxConnections = await database.query("SHOW max_connections;");
+  const activeConnections = await database.query(
+    "SELECT count(*) FROM pg_stat_activity;",
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: getVersion.rows[0].server_version,
+    max_connections: parseInt(maxConnections.rows[0].max_connections),
+    connected: parseInt(activeConnections.rows[0].count),
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
-test("should return 200 to api/v1/status", async () =>{
-  const response = await fetch("http://localhost:3000/api/v1/status")
-  expect(response.status).toBe(200)
-})
+test("should return 200 to api/v1/status", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.version).toBe("16.0");
+  expect(responseBody.max_connections).toBeGreaterThanOrEqual(100);
+  expect(responseBody.connected).toBeGreaterThanOrEqual(5);
+  expect(responseBody.connected).toBeLessThanOrEqual(
+    responseBody.max_connections,
+  );
+});
