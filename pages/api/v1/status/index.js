78d631548e518ec9211982642bae2ec85da553ba
vini051 <60819460+vini051@users.noreply.github.com>
2024-01-12 00:24:29 +0000

    refactoring status API info

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -7,14 +7,18 @@ async function status(request, response) {
   );
   const maxConnections = await database.query("SHOW max_connections;");
   const activeConnections = await database.query(
-    "SELECT count(*) FROM pg_stat_activity;",
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db'",
   );
 
   response.status(200).json({
     updated_at: updatedAt,
-    version: getVersion.rows[0].server_version,
-    max_connections: parseInt(maxConnections.rows[0].max_connections),
-    connected: parseInt(activeConnections.rows[0].count),
+    dependencies: {
+      database: {
+        version: getVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        connected: activeConnections.rows[0].count,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,15 +3,16 @@ test("should return 200 to api/v1/status", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.version).toBe("16.0");
-  expect(responseBody.max_connections).toBeGreaterThanOrEqual(100);
-  expect(responseBody.connected).toBeGreaterThanOrEqual(5);
-  expect(responseBody.connected).toBeLessThanOrEqual(
-    responseBody.max_connections,
-  );
+  const version = responseBody.dependencies.database.version;
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  const activeConnections = responseBody.dependencies.database.connected;
+
+  expect(version).toBe("16.0");
+  expect(maxConnections).toBeGreaterThanOrEqual(100);
+  expect(activeConnections).toBe(1);
+  expect(activeConnections).toBeLessThanOrEqual(maxConnections);
 });
