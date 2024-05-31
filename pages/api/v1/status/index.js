95813ca5552e0a638a7e85f0eac72a8899f8d26e
vini051 <60819460+vini051@users.noreply.github.com>
2024-05-31 15:44:24 +0000

    finishes `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,9 +6,12 @@ async function status(request, response) {
     "SELECT current_setting('server_version') as server_version;",
   );
   const maxConnections = await database.query("SHOW max_connections;");
-  const activeConnections = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db'",
-  );
+  
+  const dbname = process.env.POSTGRES_DB;
+  const activeConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbname],
+  });
 
   response.status(200).json({
     updated_at: updatedAt,
