7ef18ea2febe238bfe66932f4544ad4669115ae3
vini051 <60819460+vini051@users.noreply.github.com>
2024-06-10 20:54:58 +0000

    adds a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,21 +7,15 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'production' ? true : false,
+    ssl: getSSLvalue(),
   });
-  console.log({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  })
-  try{
+  
+  try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
-  } catch(err) {
-    console.error(err)
+  } catch (err) {
+    console.error(err);
     throw err;
   } finally {
     await client.end();
@@ -31,3 +25,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLvalue() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
