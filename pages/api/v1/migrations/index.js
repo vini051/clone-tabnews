aededbc60c69f7e746ae400c5219eb6c1e68e08d
vini051 <60819460+vini051@users.noreply.github.com>
2024-11-10 01:23:59 +0000

    adds `.end()` to the return of `infra/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -29,6 +29,6 @@ export default async function migrations(request, response) {
     }
     return response.status(200).json(migratedMigrations);
   } else {
-    return response.status(405);
+    return response.status(405).end();
   }
 }
